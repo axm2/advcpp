514353040feceb8d539179325e25d990db3a0cc2
axm2 <axmirza2@yahoo.com>
2020-02-23 16:44:39 -0500

    Experimenting with 2D before making class

--- project1/safearray.cpp
+++ project1/safearray.cpp
@@ -101,37 +101,38 @@ ostream &operator<<(ostream &os, SA<int> s)
 {
     int size = s.high - s.low + 1;
     for (int i = 0; i < size; i++)
-        cout << s.p[i] << endl;
+        cout << s.p[i];
     return os;
 };
 ostream &operator<<(ostream &os, SA<double> s)
 {
     int size = s.high - s.low + 1;
     for (int i = 0; i < size; i++)
-        cout << s.p[i] << endl;
+        cout << s.p[i];
     return os;
 };
 int main()
 {
-    SA<double> a(10), b(3, 5);
-    b[3] = 3.0;
-    b[4] = 4.0;
-    b[5] = 5.0;
-    int i;
-    for (i = 0; i < 10; i++)
-        a[i] = 10 - i;
-    cout << "printing a the first time" << endl;
-    cout << a << endl;
-    cout << "printing using []" << endl;
-    for (i = 0; i < 10; i++)
-        cout << a[i] << endl;
-    // write your own sort
-    //Sort(a,10);
-    cout << "printing a the second time" << endl;
-    cout << a << endl;
-    b[4] = 12;
-    cout << "printing b " << endl;
-    cout << b << endl;
-    a[10] = 12; // should print an error message and exit
+    SA<SA<int>> a(3);
+    SA<int> b(3), c(3), d(3);
+    a[0] = b;
+    a[1] = c;
+    a[2] = d;
+    a[0][0] = 1;
+    a[0][1] = 2;
+    a[0][2] = 3;
+    a[1][0] = 4;
+    a[1][1] = 5;
+    a[1][2] = 6;
+    a[2][0] = 7;
+    a[2][1] = 8;
+    a[2][2] = 9;
+    //b[0] = 4;
+    cout << a[0] << endl;
+    cout << a[1] << endl;
+    cout << a[2] << endl;
+    //cout << b << endl;
+
+
     return 0;
 }
