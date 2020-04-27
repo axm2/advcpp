bff70a5e6d8e723ed206c940a62dfce7befc34be
axm2 <axmirza2@yahoo.com>
2020-04-27 16:29:41 -0400

    dumb pointer implementation
    has memory leaks

--- midterm/midterm.cpp
+++ midterm/midterm.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+class block
+{
+private:
+	// total memory that block class controls
+	void *pool = new char[20000];
+	// stats
+	static int numBlocks;
+	static int szAllocated;
+	int missCounter;
+
+public:
+	// ptr to start of usable block
+	void *p = NULL;
+
+	block(int low, int high, int sizeOfT)
+	{
+		// allocate memory here
+		p=&pool;
+		cout << &pool << endl;
+	}
+};
+
+int main()
+{
+	block b(0, 5, sizeof(int));
+	//int* ptr;
+	int *ptr = (int *)b.p;
+	cout << ptr << endl;
+}
